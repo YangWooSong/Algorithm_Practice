351ba6f05087a5fa8fd98448ae1a62b6aaaf2652
양우송 <90258668+YangWooSong@users.noreply.github.com>
2025-08-06 18:53:35 +0900

    Create 11726.cpp

---
+++ BAEKJOON/Sliver3/11726.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void Init() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+}
+
+int main() {
+	Init();
+	
+	int n = 0;
+	cin >> n;
+
+	vector<long long> dp;
+	dp.resize(1001);
+	dp[1] = 1;
+	dp[2] = 2;
+
+	for (int i = 3; i <= n; ++i)
+	{
+		dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+	}
+
+	cout << dp[n];
+
+	return 0;
+}
